e13b13282e93c0987fc63f1a939b485f666c2c8e
DrRes <sinnhazime@gmail.com>
2020-08-09 16:12:19 +0900

    revert sphinx_markdown_tables

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ release = u'0.0.1'
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
-    #'sphinx_markdown_tables',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
